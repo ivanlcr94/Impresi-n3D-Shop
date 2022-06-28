d6784f76f793723a5f6ea020550faf41d87a9712
Ivan Romero <ivanlcr94@gmail.com>
2022-06-27 21:20:42 -0300

    corrección de claasName y nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,41 @@ function Navbar() {
 
     <header>  
 
-      <nav class=" navbar navbar-dark bg-dark">
+<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+  <a className="navbar-brand logo">IMPRESIÓN<span id="logo3d">3D</span> Shop</a>
+  <CartWidget />
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse listaNav " id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Servicios</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contacto</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Productos
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a className="dropdown-item" href="#">Herramientas</a></li>
+            <li><a className="dropdown-item" href="#">Juguetes</a></li>
+            <li><a className="dropdown-item" href="#">Deco</a></li>
+          </ul>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+
+ {/*      <nav class=" navbar navbar-dark bg-dark">
         <div class="container">
           <a class="navbar-brand">IMPRESIÓN<span id="logo3d">3D</span> Shop</a>
           <form class="d-flex" role="search">
@@ -15,7 +49,7 @@ function Navbar() {
             <CartWidget />
           </form>
         </div>
-      </nav>
+      </nav> */}
  
     </header>
 
